06940d9e2b7f5547891b3a321ce1a71ca3d76154
DiegoMoralesBravo <Diegomorales1359@gmail.com>
2022-10-10 09:46:24 -0500

    Funcion para evaluar

--- clase.py
+++ clase.py
@@ -1,11 +1,24 @@
 
-
+# La clase ropa con solo un metodo que recibe esos dos argumentos como indica el problema
 
 class Ropa:
 
     def descuento(self, costo, tipo):
-
+        descuento = 0
         if tipo == 'fueraDeTemporada':
-            return 'hola'
+            if costo < 500:
+                descuento = 10
+            elif costo >= 500 and costo <= 5000:
+                descuento = 20
+            else:
+                descuento = 0
         else:
-            print('probando')
+            if costo < 1000:
+                descuento = 5
+            elif costo >= 1000 and costo <= 6000:
+                descuento = 10
+
+            else:
+                descuento = 0
+
+        return descuento
--- principal.py
+++ principal.py
@@ -9,8 +9,23 @@ class TestRopa(unittest.TestCase):
     def setUp(self):
         self.ropa = Ropa()
 
-    def test_ropa(self):
-        self.assertEqual(self.ropa.descuento(7,'fueraDeTemporada'),'hola')
+    #PARA CADA PRUEBA SE LLAMA EL MEOTODO DESCUENTO Y SE LE DA DE ENTRADA 2 VALORES, EL COSTO Y SI ES DE TEMPORADA O NO, 
+    # SE REVISA QUE EL RESULTADO CORRESPONDA AL DESCUENTO QUE RETORNA LA FUNCION
+
+    def test_ropaFueraTemporada_menor500(self):
+        self.assertEqual(self.ropa.descuento(499,'fueraDeTemporada'),10)
+
+
+    def test_ropaFueraTemporada_mayor500_menor5000(self):
+        self.assertEqual(self.ropa.descuento(500,'fueraDeTemporada'),20)
+
+
+    def test_ropaTemporada_menor1000(self):
+        self.assertEqual(self.ropa.descuento(999,'temporada'),5)
+
+
+    def test_ropaTemporada_mayor1000_menor6000(self):
+        self.assertEqual(self.ropa.descuento(1000,'temporada'),10)
 
 if __name__ == "__main__":
     
