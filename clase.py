b44f58c6370f61fc089820c5c84056cc754f4b0d
DiegoMoralesBravo <Diegomorales1359@gmail.com>
2022-10-10 09:24:37 -0500

    Cosas que debe de llevar el caso de prueba

--- clase.py
+++ clase.py
@@ -1,1 +1,21 @@
-print("Hola prro")
+# Desarrolla un programa que, dado el monto de compra de ropa fuera de 
+# temporada y el monto de compra de la ropa de temporada, el programa sea 
+# capaz de mostrar el total de la compra, el descuento obtenido y el total a pagar. 
+# Implementa sólo la función para calcular el descuento de la ropa. 
+# Considera que la función deberá recibir como parámetros de entrada monto de la compra y tipo de compra.
+#  Donde monto de la compra es el costo total de la ropa de temporada o la ropa fuera de temporada (pero no ambas) y 
+#  el tipo indica si es o no de temporada. Adicionalmente deberás desarrollar (codificar) por lo 
+#  menos cuatro pruebas unitarias para validar la función.
+
+
+# QUE TIENE UN CASO DE PRUEBA
+# nombre del proyecto
+# id de caso de prueba
+# pre requisitos
+# pasos o secuencia logica
+# resultados esperados
+
+# observaciones
+
+class Ropa:
+    
